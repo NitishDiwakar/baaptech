4b48fc6e36bf15240e4568c2a21211d9545bc2da
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2022-03-06 11:03:22 +0530

    Dynamic current year

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,7 +1,8 @@
 <footer class="bg-black text-center py-5">
             <div class="container px-5">
                 <div class="text-white-50 small">
-                    <div class="mb-2">&copy; Bhartiya Awakend Azad Party 2022. All Rights Reserved.</div>
+                    <div class="mb-2">&copy; Bhartiya Awakend Azad Party <?php echo date("Y"); ?>
+. All Rights Reserved.</div>
                     <a href="#!">Privacy</a>
                     <span class="mx-1">&middot;</span>
                     <a href="#!">Terms</a>
