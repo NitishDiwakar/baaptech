dc82de06262535dc7363d9175807b3d45b05158e
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2022-03-06 10:57:20 +0530

    pre footer sliced, social links added

--- templates/Home/index.php
+++ templates/Home/index.php
@@ -170,15 +170,8 @@ BAAP token is listed on pancake swap, our smart contract address is
         </section>
         
         <!-- App badge section-->
-        <section class="bg-gradient-primary-to-secondary" id="download">
-            <div class="container px-5">
-                <h2 class="text-center text-white font-alt mb-4">Join our community now!</h2>
-                <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
-                    <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
-                    <a href="#!"><img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
-                </div>
-            </div>
-        </section>
+        <?= $this->element('pre_footer') ?>
+        
         <!-- Footer-->
         <?= $this->element('suggestion') ?>
         <?= $this->element('footer') ?>
---
+++ templates/element/pre_footer.php
@@ -0,0 +1,15 @@
+<section class="bg-gradient-primary-to-secondary" id="download">
+            <div class="container px-5">
+                <h2 class="text-center text-white font-alt mb-4">Join our community now!</h2>
+                <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                    <a class="me-lg-3 mb-4 mb-lg-0" href="https://twitter.com/PumpNation4" target="_blank" title="Follow us on twitter">
+                        <!-- <img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /> -->
+                        <i class="text-white bi bi-twitter" style="font-size: 30px"></i>
+                    </a>
+                    <a href="https://t.me/pumpnatio" target="_blank" title="Join our telegram group">
+                        <!-- <img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /> -->
+                        <i class="text-white bi bi-telegram" style="font-size: 30px"></i>
+                    </a>
+                </div>
+            </div>
+</section>
