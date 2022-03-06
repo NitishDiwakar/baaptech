7a8da7e868ba5bf46b61ce5e5da209f39e65c893
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2022-03-06 10:59:35 +0530

    Updated

--- templates/element/pre_footer.php
+++ templates/element/pre_footer.php
@@ -2,7 +2,9 @@
             <div class="container px-5">
                 <h2 class="text-center text-white font-alt mb-4">Join our community now!</h2>
                 <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
-                    <a class="me-lg-3 mb-4 mb-lg-0" href="https://twitter.com/PumpNation4" target="_blank" title="Follow us on twitter">
+                    <div class="row">
+                        <div class="col-12">
+                            <a class="me-lg-3 mb-4 mb-lg-0" href="https://twitter.com/PumpNation4" target="_blank" title="Follow us on twitter">
                         <!-- <img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /> -->
                         <i class="text-white bi bi-twitter" style="font-size: 30px"></i>
                     </a>
@@ -10,6 +12,8 @@
                         <!-- <img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /> -->
                         <i class="text-white bi bi-telegram" style="font-size: 30px"></i>
                     </a>
+                        </div>
+                    </div>
                 </div>
             </div>
 </section>
